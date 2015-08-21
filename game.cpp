244521cd402f05d4414dd643ab1def11390acab7
prophet-leong <leongjunxiang1@hotmail.com>
2015-08-21 15:47:53 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -29,8 +29,8 @@ void storepoints();
 void printpoints();
 short data1[10];
 void win();
-void playwin();
-void stopwin();
+void playsound();
+void stopsound();
 // Game specific variables here
 COORD charLocation;
 COORD charLocation2;
@@ -55,7 +55,8 @@ void init()
 void shutdown()
 {
     // Reset to white text on black background
-    colour(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+        colour(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+ 
     console.clearBuffer();
 }
 /*
@@ -88,25 +89,26 @@ void getInput()
 void update(double dt)
 {
     // get the delta time
-	if(next!=0 && next!= 16 && next!=99)
-	{
-		elapsedTime += dt;
+	if(next!=0 && next!= 16 && next!=99){
+    elapsedTime += dt;
 	}
     deltaTime = dt;
-    switch(next)
-    {
+        switch(next)
+        {
 		case 16:win();break;
 		case 99:help();break;
 		case 100:lose();elapsedTime=0;break;
-		case 0:menu();sidemenu();break;
-		case 1:level1();break;
-		case 2:level2();break;
-		case 3:level3();break;
-		case 4:level4();break;
-		case 5:level5();break;
-		case 6:level6();break;
-		case 7:level7();break;
-    }
+        case 0:menu();sidemenu();
+                break;
+        case 1:level1();break;
+        case 2:level2();break;
+        case 3:level3();break;
+        case 4:level4();break;
+        case 5:level5();break;
+        case 6:level6();break;
+        case 7:level7();break;
+		
+        }
     processUserInput(); // checks if you should change states or do something else with the game, e.g. pause, exit
     moveCharacter();    // moves the character, collision detection, physics, etc
     // sound can be played here too.
@@ -229,7 +231,7 @@ void moveCharacter()
 	{
 		Beep(1440,30);
 		next = 0;
-		stopwin();
+		stopsound();
 		charLocation.X = 24;charLocation.Y = 9;charLocation2.X = 24;charLocation2.Y = 9;
 	}
 	else if ((keyPressed[K_SPACE]) && (next == 99))		
@@ -244,9 +246,9 @@ void processUserInput()
 {
     // quits the game if player hits the escape key
     if (keyPressed[K_ESCAPE])
-    {
-	    g_quitGame = true;
-    }
+        {
+        g_quitGame = true;
+        }
 }
  
 void clearScreen()
@@ -259,53 +261,55 @@ void renderMap()
 {
     // Set up sample colours, and output shadings
     const WORD colors[] = {
-        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
-        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
+        0x07, 0x0F, 0x08, 0x02, 0x0B, 0x04,
+        0x05, 0x07, 0x0F, 0x08, 0x02, 0x0B,
+		0x0B, 0x04, 0x05,
 		};
         for (unsigned int i=0;i<24;++i)
         {
-			for (unsigned int c=0;c<71;++c)
-			{
-				if(level[i][c] == '#')
-				{
-					unsigned char a = 219;
-					if(next>0 && next<99 && next!=16)
-					{
-						console.writeToBuffer(c,i+1,a,0x23);
-					}
-					else
-					{
-						console.writeToBuffer(c,i+1,a,0x00);
-					}
-				}
-				else
-				{               
-					   console.writeToBuffer(c,i+1,level[i][c]);
-				}
-			}
+        for (unsigned int c=0;c<71;++c)
+        {
+                if(level[i][c] == '#')
+                {
+                        unsigned char a = 219;
+						if(next>0 && next<99 && next!=16)
+						{
+                        console.writeToBuffer(c,i+1,a,colors[next-1]);
+						}
+						else
+						{
+						 console.writeToBuffer(c,i+1,a,0x00);
+						}
+                }
+                else
+                {
+                               
+                        console.writeToBuffer(c,i+1,level[i][c]);
+                }
+        }
         }
         if(next >0 && next != 100 && next != 16 && next!=99)
         {
-            for (unsigned int i=0;i<23;++i)
-            {
-                for(unsigned int c=0;c<10;++c)
+                for (unsigned int i=0;i<23;++i)
                 {
-                    if(side[i][c] == '#')
-					{
-						unsigned char a = 219;
-                        console.writeToBuffer(c+70,i+1,a,0x00);//side menu
-                    }
-                    else
-                    {
-                        console.writeToBuffer(c+70,i+1,side[i][c]);
-                    }
+                        for(unsigned int c=0;c<10;++c)
+                        {
+                                if(side[i][c] == '#')
+								{
+									unsigned char a = 219;
+                                    console.writeToBuffer(c+70,i+1,a,0x00);//side menu
+                                }
+                                else
+                                {
+                                        console.writeToBuffer(c+70,i+1,side[i][c]);
+                                }
+                        }
                 }
-            }
         }
 
 	if(next == 16)
 	{
-		printpoints();
+	printpoints();
 	}
 }
 void renderCharacter()
@@ -361,7 +365,7 @@ void storepoints()
 {
 
 	int c=0;
-	playwin();
+	playsound();
 	int a =static_cast<int>(elapsedTime);
 	std::fstream fs;
 	fs.open ("scoreboard.txt", std::fstream::in | std::fstream::out | std::fstream::app);
@@ -384,12 +388,12 @@ void storepoints()
 		{
 			for(short b = a;b<999-1;b++)
 			{
-				if(bubble[a]<bubble[b+1])
-				{
+				 if(bubble[a]<bubble[b+1])
+				 {
 					temp=bubble[a];
 				    bubble[a]=bubble[b+1];
 					bubble[b+1]=temp; 
-				}
+				 }
 			}
 		}
 		for(short a = 0;a<10;a++)
@@ -449,21 +453,25 @@ void win()
         ,       {" ##                                                                 ##"}
         ,       {" ##                                                                 ##"}
         ,       {" #####################################################################"}
-		,       {" #####################################################################"}};
+        ,       {" #####################################################################"}
+
+
+       
+        };
  
         for(int i=0;i<24;++i)
         {
-            for(int c=0;c<71;++c)
-            {
-                level[i][c] = win[i][c];
-            }
+                for(int c=0;c<71;++c)
+                {
+                        level[i][c] = win[i][c];
+                }
         }
 }
-void playwin()
+void playsound()
 {
-	PlaySound(TEXT("win.wav"),NULL,SND_FILENAME|SND_ASYNC);
+		PlaySound(TEXT("win.wav"),NULL,SND_FILENAME|SND_ASYNC);
 }
-void stopwin()
+void stopsound()
 {
 	PlaySound(NULL,NULL,0);
 }
