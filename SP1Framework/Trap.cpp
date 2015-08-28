c9c19bfd9c76d1018bcaf2ce732fa50c6c2ffa44
BlazeIgnite <teckling98@hotmail.com>
2015-08-28 10:31:32 +0800

    New trap.cpp and trap.h

---
+++ SP1Framework/Trap.cpp
@@ -0,0 +1,6 @@
+#include "Trap.h"
+#include <iostream>
+
+void movingTrapUD(unsigned int x, unsigned int y, bool up)
+{
+}
---
+++ SP1Framework/Trap.h
@@ -0,0 +1,13 @@
+#ifndef _TRAP_H
+#define _TRAP_H
+
+void movingTrapUD(unsigned int x, unsigned int y, bool up);
+void movingTrapLR(unsigned int x, unsigned int y, bool left);
+
+
+
+
+
+
+
+#endif
